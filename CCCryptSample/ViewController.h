542004c36401f7a208221d4c29b7b923c3ca1930
Hank <bbkaf@hotmail.com>
2018-07-04 11:25:51 +0800

    Initial Commit

---
+++ CCCryptSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCCryptSample
+//
+//  Created by HankTseng on 2018/7/4.
+//  Copyright © 2018年 HyerTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
